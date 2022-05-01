334fb687b03c5fad0a7b1b8b727639f4d395580b
awc1437332 <awc1437@rit.edu>
2022-05-01 19:59:46 -0400

    added web components

---
+++ src/My-footer.js
@@ -0,0 +1,35 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<style>
+footer {
+    font-family: 'Press Start 2P', cursive;
+    font-size: 24px;
+}
+</style>
+<footer class="footer py-4">
+    &copy Andrew Carbonari 2022 & &copy Louis D'Angelo
+</footer>
+`;
+
+
+class MyFooter extends HTMLElement {
+    constructor(){
+        super();
+
+        // 1 - Attach a shadow DOM tree to this instance - this creates `.shadowRoot` for us
+        this.attachShadow({mode: "open"});
+
+        // 2 - clone `template` and append it
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.footer = this.shadowRoot.querySelector("footer");
+    }
+} // end class
+
+customElements.define('my-footer', MyFooter);
---
+++ src/My-header.js
@@ -0,0 +1,77 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<style>
+    #title {
+        font-family: 'Press Start 2P', cursive;
+        font-size: 36px;
+    }
+</style>
+
+<header>
+    <nav class="navbar py-4 pl-4 has-background-danger-light">
+        <div class="navbar-brand">
+            <h1 id="title">Jello Jump!</h1>
+            <a class="navbar-item" href="about.html">
+                <i class="fab fa-d-and-d fa-2x">
+            </i></a>
+            <a class="navbar-burger" id="burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
+        </div>
+
+        <div class="navbar-menu" id="nav-links">
+            <a href="about.html" class="navbar-item">About</a>
+            <a href="app.html" class="navbar-item">Apps</a>
+            <a href="documentation.html" class="navbar-item">Documentation</a>
+        </div>
+    </nav>
+</header>
+`;
+
+class MyHeader extends HTMLElement {
+    constructor() {
+        super();
+
+        // 1 - Attach a shadow DOM tree to this instance - this creates `.shadowRoot` for us
+        this.attachShadow({ mode: "open" });
+
+        // 2 - clone `template` and append it
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.nav = this.shadowRoot.querySelector("nav");
+
+        // mobile menu
+        this.burgerIcon = this.shadowRoot.querySelector("#burger");
+        this.navbarMenu = this.shadowRoot.querySelector("#nav-links");
+    }
+
+    /**
+     * This function is calling connected call back to initialize the burger icon menu
+     */
+    connectedCallback() {
+        this.burgerIcon.addEventListener('click', () => {
+            this.navbarMenu.classList.toggle('is-active');
+        })
+    }
+
+    /**
+     * This method runs when the component is destroyed
+     */
+    disconnectedCallback() {
+        this.burgerIcon.removeEventListener('click', () => {
+            this.navbarMenu.classList.toggle('is-not-active');
+        })
+    }
+
+} // end class
+
+customElements.define('my-header', MyHeader);
