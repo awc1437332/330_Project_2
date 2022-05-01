dde8e2b1d20a19b837eec06d72b3e082ff75df5a
lpd8987 <lpd8987@rit.edu>
2022-05-01 09:26:23 -0400

    Game basically finished
    BIG PUSH

--- src/loader.js
+++ src/loader.js
@@ -3,7 +3,23 @@ import { loadImages } from './helpers.js';
 
 const imageSources = {
 	//evil1: 'images/Dr-evil.jpg',
-	player: 'images/JelloBoySheet.png' //player spritesheet should be first thing loaded for code clarity
+	player: 'images/JelloBoySheet.png', //player spritesheet should be first thing loaded for code clarity
+	kingPlayer: 'images/KingJelloBoySheet.png',
+	background: 'images/Background.png',
+	baseLevel: 'images/BaseLevel.png',
+	titleScreen: 'images/TitleScreen.png',
+	levelOne: 'images/Level1.png',
+	levelTwo: 'images/Level2.png',
+	levelThree: 'images/Level3.png',
+	levelFour: 'images/Level4.png',
+	levelFive: 'images/Level5.png',
+	levelSix: 'images/Level6.png',
+	levelSeven: 'images/Level7.png',
+	levelEight: 'images/Level8.png',
+	levelNine: 'images/Level9.png',
+	levelTen: 'images/Level10.png',
+	crown: 'images/crown.png',
+	door: 'images/ExitDoor.png'
 };
 
 // loadImages(imageSourcesObject,callback);
--- src/main.js
+++ src/main.js
@@ -4,23 +4,57 @@ import { getRandomUnitVector } from './utilities.js';
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const screenWidth = 600;
-const screenHeight = 400;
+const screenWidth = canvas.width;
+const screenHeight = canvas.height;
+
+const text = document.querySelector("#flavor-text");
+const stats = document.querySelector("#player-stats");
+
+let currentLevel = 1;
+//#region Variables
+let stillLooping = false;
+
 
 //Player Data
 let playerImage;
+let unlockableImage;
 let currentPlayerAnimation;
 let currentAnimationFrame = 0;
 
+
+
 let playerSprite; //the sprite object
+let basePlayerSprite;
+let altPlayerSprite;
+
 let playerCenter; //location vector
 
 let playerObj; //physics object
 
+let crownsCollected = 0;
+
 //Physics Variables
-let Engine, Render, Runner, Bodies, Composite;
+let Engine, Render, Runner, Body, Bodies, Composite, Collision;
+let render;
+let engine;
+
+let boxColliders = [];
+let crowns = [];
+let doorObj;
 
 let FrameCounter = 0;
+
+let titleImage;
+let backgroundImg;
+let levelImgs;
+let currentLevelImage;
+let crownImg;
+let doorImg;
+
+//JSON Level Data and Local Storage Data
+let JSONData;
+let localData;
+const playerKey = "playerData";
 
 //#region Player Animations
 const playerIdle = [
@@ -81,129 +115,221 @@ const playerLand = [
 ];
 //#endregion
 
+//level data
+let baseLevelImage;
 
-//Event Handlers (single keypress)
-let aKeyPressed = false;
-let dKeyPressed = false;
-let wKeyPressed = false;
+//Event Handlers (single keypress) - listen for input and set bool
+//Handles both WASD and Arrow Keys
+let leftInputPressed = false;
+let rightInputPressed = false;
+let topInputPressed = false;
+//#endregion
 
-document.addEventListener('keydown', function(event) {
-    switch(event.key){
-        case 'a':
-            playerObj.force.x = -.01;
-            break;
+//#region Functions
 
-        case 'd':
-            playerObj.force.x = .01;
-            break;
-
-        case 'w':
-            playerObj.force.y = -.01;
-            break;
-    }
-});
-
-document.addEventListener('keyup', function(event) {
-    switch(event.key){
-        case 'a':
-            playerObj.force.x = 0;
-            break;
-        case 'd':
-            playerObj.force.x = 0;
-            break;
-        case 'w':
-            playerObj.force.y = 0;
-            break;
-    }
-});
-
-
-function init(imgData)
+//Parse level JSON data using fetch
+function fetchData()
 {
-    playerImage = imgData.player; //player spritesheet should be the first image loaded
-    currentPlayerAnimation = playerFall;
+    let fetchPromise = async () =>{
+        let levelData = await fetch("data/level-data.json");
 
-    playerSprite = new ImageSprite(screenWidth/2, screenHeight/2, getRandomUnitVector(), 0, 32, 32, playerImage, "player");
-    playerCenter = {x:playerSprite.x + playerSprite.width/2, y:playerSprite.y + playerSprite.height/2}
+        if(!levelData.ok){
+            throw new Error(`Error getting races: status ${response.status}`);
+        }
 
-    initPhysics(playerCenter);
-    
-    loop();
+        let levelJSON = await levelData.json();
+
+
+        //store data in global variable
+        JSONData = levelJSON;
+        console.log(JSONData);
+    };
+
+    fetchPromise().catch(e => {
+        console.log(`in catch with e = ${e}`);
+    });
 }
 
+//Setup data using loaded images, initialize main menu
+function init(imgData)
+{
+    //localStorage.clear();
+
+    //Fetch only if data is not set
+    if(!JSONData)
+    {
+        fetchData();
+    }
+
+    //load in player data from local storage
+    localData = JSON.parse(localStorage.getItem(playerKey));
+    if(!localData)
+    {
+        localData = 
+        {
+            numCrowns:0,
+            bestTime:0
+        }
+        localStorage.setItem(playerKey, JSON.stringify(localData));
+        //JSON.parse(localData);
+    }
+
+    //initialize variables using image data
+    playerImage = imgData.player; //player spritesheet should be the first image loaded
+    unlockableImage = imgData.kingPlayer;
+
+    backgroundImg = imgData.background;
+    titleImage = imgData.titleScreen;
+    baseLevelImage = imgData.baseLevel;
+
+    levelImgs = []; //add level images into an array for easy reference
+    levelImgs.push(imgData.levelOne);
+    levelImgs.push(imgData.levelTwo);
+    levelImgs.push(imgData.levelThree);
+    levelImgs.push(imgData.levelFour);
+    levelImgs.push(imgData.levelFive);
+    levelImgs.push(imgData.levelSix);
+    levelImgs.push(imgData.levelSeven);
+    levelImgs.push(imgData.levelEight);
+    levelImgs.push(imgData.levelNine);
+    levelImgs.push(imgData.levelTen);
+
+    currentLevelImage = levelImgs[currentLevel-1];
+
+    crownImg = imgData.crown;
+    doorImg = imgData.door;
+
+    //Player animation
+    currentPlayerAnimation = playerFall;
+
+    //Player sprites
+    basePlayerSprite = new ImageSprite(screenWidth/2, screenHeight/2, getRandomUnitVector(), 0, 32, 32, playerImage, "player");
+    altPlayerSprite = new ImageSprite(screenWidth/2, screenHeight/2, getRandomUnitVector(), 0, 32, 32, unlockableImage, "player");
+
+    playerSprite = basePlayerSprite;
+
+    //Get player character's center point
+    playerCenter = {x:playerSprite.x + playerSprite.width/2, y:playerSprite.y + playerSprite.height/2}
+
+    //Draw Main Menu
+    ctx.drawImage(titleImage, 0, 0);
+
+    document.addEventListener('keypress', function(event) {
+        switch(event.key){
+            case 'Enter':
+                setupGame();
+                loop();
+                break;
+        }
+    });
+
+    if(stats){
+        stats.innerHTML = `Best time: _ | Total Crowns Collected: ${localData.numCrowns}`;
+    }
+}
+
+//game loop
 function loop()
 {
+    //because request animation frame is so finicky
+    if(!stillLooping)
+    {
+        return;
+    }
+
     //Start loop
     requestAnimationFrame(loop);
 
-    //Fill background
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, screenWidth, screenHeight);
+    //Draw the objects in the level
+    drawLevel();
+    updatePlayer();
 
-    //Draw Ground
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, screenHeight - 35, screenWidth + 30, 60);
-
-    ctx.fillRect(screenWidth - 30, 0, 60, screenHeight);
-    ctx.fillRect(-30, 0, 60, screenHeight);
-
-    ctx.fillRect(0, 0, screenWidth + 30, 60);
-
-    //Draw the player
-    playerSprite.draw(ctx, currentPlayerAnimation[currentAnimationFrame]);
-
-    //Update player position and animation
-    playerSprite.x = playerObj.position.x;
-    playerSprite.y = playerObj.position.y;
-
-    updatePlayerAnimation(playerObj);
-
-    //update the animation asynchronously
-    if(FrameCounter == 10){
+    //update the animation "asynchronously"
+    if(FrameCounter == 5){
         currentAnimationFrame = (currentAnimationFrame+1) % currentPlayerAnimation.length;
         FrameCounter = 0;
     }
-    
-    console.log(playerObj.velocity);
     FrameCounter++;
 
-    //console.log(FrameCounter);
+    //Check if the player is colliding with a crown pickup
+    if(crowns.length > 0) //preliminary check to see if there are items in the crowns array
+    {
+        for(let i = 0; i < crowns.length; i++)
+        {
+            if(Collision.collides(playerObj, crowns[i]))
+            {
+                //crownsCollected++;
+                //Adjust local data
+                localData.numCrowns++;
+                localStorage.setItem(playerKey, JSON.stringify(localData));
+                stats.innerHTML = `Best time: _ | Total Crowns Collected: ${localData.numCrowns}`;
+                Composite.remove(engine.world, crowns[i]);
+                crowns.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    if(Collision.collides(playerObj, doorObj)){
+        //only move to next level if there is a next level to move to
+        if (currentLevel < 10){
+            currentLevel++;
+            setupLevel(currentLevel);
+        }
+        else{
+            returnToMainMenu();
+        }
+    }
+
+    //For debug purposes
+    Render.lookAt(render, playerObj, {x:100, y:150});
 }
 
+//Helper Functions
+//Setup Matter.js, add bounding box to scene, and add player collider that will not rotate
+//(Debug: adds Matter.js physics renderer)
 function initPhysics(playerCenter)
 {
     Engine = Matter.Engine;
     Render = Matter.Render;
     Runner = Matter.Runner;
+    Body = Matter.Body;
     Bodies = Matter.Bodies;
     Composite = Matter.Composite;
+    Collision = Matter.Collision;
+
 
     // create an engine
-    let engine = Engine.create();
+    engine = Engine.create();
 
-    //let render = Render.create({
-    //    element: document.body,
-    //    engine: engine
-    //});
+    if(!render) //make sure only one renderer is ever on screen
+    {
+        render = Render.create({
+            element: document.body,
+            engine: engine
+        });
+    }
 
     // create two boxes and a ground
     playerObj = Bodies.rectangle(playerCenter.x, playerCenter.y, 16, 16);
     playerObj.friction = 0;
-    console.log(playerObj);
+    playerObj.mass = 1;
 
-    let ground = Bodies.rectangle(screenWidth/2, screenHeight - 30, screenWidth, 60, { isStatic: true });
+    Body.setInertia(playerObj, Infinity); //So the physics object does not rotate and cause any abnormalities with animation
+    //console.log(playerObj);
 
-    let wall1 = Bodies.rectangle(screenWidth - 30, screenHeight/2, 60, screenHeight, { isStatic: true });
-    let wall2 = Bodies.rectangle(0, screenHeight/2, 60, screenHeight, { isStatic: true });
-
-    let ceiling = Bodies.rectangle(screenWidth/2, 0, screenWidth, 60, { isStatic: true });
+    //Create bounding box
+    addBoxCollider(screenWidth/2, screenHeight - 16, screenWidth, 32);
+    addBoxCollider(screenWidth + 16, screenHeight/2, 32, screenHeight);
+    addBoxCollider(-16, screenHeight/2, 32, screenHeight);
+    addBoxCollider(screenWidth/2, -16, screenWidth, 32);
 
     // add all of the bodies to the world
-    Composite.add(engine.world, [playerObj, ground, wall1, wall2, ceiling]);
+    Composite.add(engine.world, [playerObj]);
 
     // run the renderer
-    //Render.run(render);
-
+    Render.run(render);
+    
     // create runner
     let runner = Runner.create();
 
@@ -211,9 +337,100 @@ function initPhysics(playerCenter)
     Runner.run(runner, engine);
 }
 
-function updatePlayerAnimation(playerObj)
+//Adds Control Event Listeners, begins game loop
+function setupGame()
 {
+    stillLooping = true;
+    //initPhysics(playerCenter);
 
+    //Set initial player sprite
+    if(localData.numCrowns > 14)
+    {
+        playerSprite = altPlayerSprite;
+    }
+    else
+    {
+        playerSprite = basePlayerSprite;
+    }
+
+    //Add Event Listeners
+    document.addEventListener('keypress', function(event) {
+        switch(event.key){
+            case 'a':
+            case "ArrowLeft":
+                leftInputPressed = true;
+                break;
+    
+            case 'd':
+            case "ArrowRight":
+                rightInputPressed = true;
+                break;
+    
+            case 'w': //W is the only key that doesnt require constant key press
+            case "ArrowUp":
+                topInputPressed = true;
+                Body.setVelocity(playerObj, {x:playerObj.velocity.x, y:-10})
+                break;
+        }
+    });
+    
+    document.addEventListener('keyup', function(event) {
+        switch(event.key){
+            case 'a':
+            case "ArrowLeft":
+                leftInputPressed = false;
+                break;
+    
+            case 'd':
+            case "ArrowRight":
+                rightInputPressed = false;
+                break;
+    
+            case 'w': //W is the only key that doesnt require constant key press
+            case "ArrowUp":
+                topInputPressed = false;
+                break;
+        }
+    });
+
+    setupLevel(currentLevel);
+}
+
+//Draw the player
+function updatePlayer()
+{
+    //Draw the player
+    playerSprite.draw(ctx, currentPlayerAnimation[currentAnimationFrame]);
+
+    //Update player position and animation
+    playerSprite.x = playerObj.position.x - playerSprite.width/2;
+    playerSprite.y = playerObj.position.y - playerSprite.width/2;
+
+    updatePlayerAnimation(playerObj);
+
+    //Update player velocity based on keypresses
+    if(leftInputPressed && !rightInputPressed) //Move Left
+    {
+        Body.setVelocity(playerObj, {x:-5, y:playerObj.velocity.y});
+    }
+
+    if(!leftInputPressed && rightInputPressed) //Move Right
+    {
+        Body.setVelocity(playerObj, {x:5, y:playerObj.velocity.y});
+    }
+
+    if((leftInputPressed && rightInputPressed) || (!leftInputPressed && !rightInputPressed)) //Dont move if there are no keys down or both A and D are pressed simultaneously
+    {
+        Body.setVelocity(playerObj, {x:0, y:playerObj.velocity.y});
+    }
+}
+
+//Helper function to update player's animation to prevent cluttering the code too much
+function updatePlayerAnimation(playerObj)
+{   
+    //console.log(playerObj.velocity);
+
+    //Player moving left or right
     if(playerObj.velocity.x > 2)
     {
         currentPlayerAnimation = playerMoveRight;
@@ -226,41 +443,176 @@ function updatePlayerAnimation(playerObj)
         return;
     }
 
-    //player is idle when: completely stationary and not preparing to jump or land
+    if(currentPlayerAnimation == playerFall && topInputPressed)
+    {
+        currentPlayerAnimation = playerJump;
+        return;
+    }
+
+
+    //player is landing when:
+    if(currentPlayerAnimation == playerFall && playerObj.velocity.y <= 0)
+    {
+        currentPlayerAnimation = playerLand;
+        currentAnimationFrame = 0;
+        return;
+    }
+
+    if(currentPlayerAnimation == playerLand && currentAnimationFrame >= 5)
+    {
+        currentPlayerAnimation = playerIdle;
+        return;
+    }
+    else if (currentPlayerAnimation == playerLand)
+    {
+        return;
+    }
+
+    //player is falling when: velocity.y is bigger than 0 and moving at a total speed faster than 1
+    if(playerObj.velocity.y > 1 && playerObj.speed > 1)
+    {
+        currentPlayerAnimation = playerFall;
+        return;
+    }
+
     if(playerObj.speed < 2)
     {
        currentPlayerAnimation = playerIdle; 
        return;
     }
 
-    //player is falling when: velocity.y is bigger than 0 and moving at a total speed faster than 1
-    if(playerObj.velocity.y > 0 && playerObj.speed > 1)
-    {
-        currentPlayerAnimation = playerFall;
-        return;
-    }
-    
-    //player is landing when:
-    if(currentPlayerAnimation == playerFall && playerObj.speed < 1)
-    {
-        currentPlayerAnimation = playerLand;
-        currentAnimationFrame = 0;
-        return;
-    }
-    
-    if(currentPlayerAnimation == playerLand && currentAnimationFrame != 5)
-    {
-        currentPlayerAnimation = playerIdle;
-        return;
-    }
-
-
     //player is jumping when:
-    if(playerObj.velocity.y < 0 && playerObj.speed > 1)
+    if(playerObj.velocity.y <= 0)
     {
         currentPlayerAnimation = playerJump;
         return;
     }
-
- 
 }
+
+//#region Helper Collider add Methods
+function addBoxCollider(x, y, width, height)
+{
+    //reference to new box
+    let newBox = Bodies.rectangle(x, y, width, height, { isStatic: true });
+
+    //add to collection of all boxes
+    boxColliders.push(newBox);
+
+    //add to physics renderer
+    Composite.add(engine.world, [newBox]);
+
+    console.log(newBox);
+}
+
+function addCrown(x, y)
+{
+    let newCrown = Bodies.rectangle(x, y, 32, 32, {isStatic: true});
+    crowns.push(newCrown);
+
+    Composite.add(engine.world, [newCrown]);
+
+    //console.log(newCrown);
+}
+
+function setExit(x, y)
+{
+    doorObj = Bodies.rectangle(x, y, 32, 32, {isStatic: true});
+    Composite.add(engine.world, [doorObj]);
+    
+    //console.log(doorObj);
+}
+//#endregion
+
+
+//#region Level Functions
+//Uses level JSON data to add platforms, pickups, and the level exit
+function setupLevel(levelNum)
+{
+    //clear any current level data
+    clearLevel();
+    initPhysics(playerCenter);
+
+    currentLevelImage = levelImgs[levelNum - 1];
+
+    let levelObj = JSONData.levels[levelNum - 1];
+
+    text.innerHTML = `Level ${levelNum}: ${levelObj.title}`; 
+
+    //Move the player to the start position
+    Body.setPosition(playerObj, levelObj.playerPosition);
+
+    //Add the physics bodies
+    for(let i = 0; i < levelObj.tiles.length; i++)
+    {
+        addBoxCollider(levelObj.tiles[i].x, levelObj.tiles[i].y, levelObj.tiles[i].width, levelObj.tiles[i].height);
+    }
+
+    //Add the crowns
+    for(let i = 0; i < levelObj.crowns.length; i++)
+    {
+        addCrown(levelObj.crowns[i].x, levelObj.crowns[i].y);
+    }
+
+    //Add the exit door
+    setExit(levelObj.exit.x, levelObj.exit.y);
+}
+
+//Clear all objects (INCLUDING PLAYER) from the physics simulation
+function clearLevel()
+{
+    //preliminary check to make sure the level actually has colliders to remove
+    if(boxColliders.length < 1){
+        return;
+    }
+
+    Composite.clear(engine.world, false);
+    boxColliders = [];
+    crowns = [];
+    doorObj = null;
+}
+
+//Draw all physics components in the physics simulation
+function drawLevel()
+{
+    //background
+    ctx.fillStyle = 'green';
+    //ctx.fillRect(0, 0, screenWidth, screenHeight);
+    ctx.drawImage(backgroundImg, 0, 0);
+    
+    //environment (for debugging purposes only)
+    ctx.fillStyle = "black";
+    for (let i = 0; i < boxColliders.length; i++)
+    {
+        //x, y, width, height
+        ctx.fillRect(boxColliders[i].vertices[0].x, boxColliders[i].vertices[0].y, boxColliders[i].vertices[1].x - boxColliders[i].vertices[0].x, boxColliders[i].vertices[2].y - boxColliders[i].vertices[0].y);
+    }
+
+    //Draw BaseLevel
+    //ctx.drawImage(baseLevelImage, 0, 0);
+
+    //Draw CurrentLevel
+    ctx.drawImage(currentLevelImage, 0, 0);
+
+    //Draw Crowns
+    for(let i = 0; i < crowns.length; i++)
+    {
+        ctx.drawImage(crownImg, crowns[i].vertices[0].x, crowns[i].vertices[0].y);
+    }
+
+    //Draw Exit
+    ctx.drawImage(doorImg, doorObj.vertices[0].x, doorObj.vertices[0].y);
+}
+//#endregion
+
+
+function returnToMainMenu()
+{
+    cancelAnimationFrame(loop);
+    currentLevel = 1;
+    stillLooping = false;
+
+    ctx.drawImage(titleImage, 0, 0);
+    text.innerHTML = `Congratulations! Your time was _. Press 'Enter' to play again!`;
+
+}
+//#endregion
